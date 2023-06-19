fda7b9d498ab9775be357e7fa7bc6c63456770e4
amaninderpreetsingh <51932247+amaninderpreetsingh@users.noreply.github.com>
2023-06-18 19:44:46 -0700

    new text

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import home from './home'
 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>TEST pr</Text>
       <StatusBar style="auto" />
     </View>
   );
